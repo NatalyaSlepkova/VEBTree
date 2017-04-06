5b55cea0de1abca4ed24f654dcd8e1a11fe2c365
NatalyaSlepkova <slepkovanatal@mail.ru>
2017-04-06 21:18:50 +0300

    Update veb.h

--- veb.h
+++ veb.h
@@ -4,9 +4,9 @@ template<unsigned int S>
 class AbstractVEBTree {
 public:
 	virtual void add(unsigned long long x) = 0;
-	//virtual void remove(unsigned long long x) = 0;
+	virtual void remove(unsigned long long x) = 0;
 	virtual unsigned long long next(unsigned long long x) const = 0;
-	//virtual unsigned long long prev(unsigned long long x) const = 0;
+	virtual unsigned long long prev(unsigned long long x) const = 0;
 	virtual unsigned long long getMin() const = 0;
 	virtual unsigned long long getMax() const = 0;
 };
