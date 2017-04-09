b11a62b35363591f1f770454faf438096fa6cab3
NatalyaSlepkova <slepkovanatal@mail.ru>
2017-04-09 23:33:27 +0300

    Update veb.cpp

--- veb.cpp
+++ veb.cpp
@@ -2,28 +2,12 @@
 #include <fstream>
 #include <vector>
 #include <string>
-//#include "veb.h"
+#include "veb.h"
 #include <algorithm>
 #include <memory.h>
 #include <unordered_map>
 
 using namespace std;
-
-unsigned long long const NO = -1;
-
-template<unsigned int S>
-class AbstractVEBTree {
-public:
-	virtual void add(unsigned long long x) = 0;
-	virtual void remove(unsigned long long x) = 0;
-	virtual unsigned long long next(unsigned long long x) const = 0;
-	virtual unsigned long long prev(unsigned long long x) const = 0;
-	virtual unsigned long long getMin() const = 0;
-	virtual unsigned long long getMax() const = 0;
-};
-
-template<unsigned int S>
-struct VEBTree;
 
 template <unsigned int S>
 struct VEBTree : AbstractVEBTree<S>
